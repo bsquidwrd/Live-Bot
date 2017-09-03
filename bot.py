96cbb657301c7b68cb398787bfd36a0ff4dfb795
Brandon <git@bsquid.io>
2017-09-02 21:13:42 -0700

    Testing something

--- bot.py
+++ bot.py
@@ -14,6 +14,7 @@ import importlib
 
 import web.wsgi
 from cogs.utils import logify_exception_info, logify_dict
+print(os.environ['LIVE_BOT_CLIENT_ID'])
 
 debug_mode = os.getenv('LIVE_BOT_DEBUG_MODE', 'true')
 if not isinstance(debug_mode, bool):
