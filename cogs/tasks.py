5ef34d5a33eeb85fd0c88532bd637e52078d33f7
Brandon <git@bsquid.io>
2017-11-15 09:54:26 -0800

    Added new logic to prevent recent errors ocurring

--- cogs/tasks.py
+++ cogs/tasks.py
@@ -110,7 +110,9 @@ class Tasks:
                 for stream in result_json['streams']:
                     if stream is not None:
                         if stream['stream_type'] == 'live':
-                            twitch = TwitchChannel.objects.get(id=stream['channel']['_id'])
+                            twitch, created = TwitchChannel.objects.get_or_create(id=stream['channel']['_id'], defaults={'name': stream['channel']['name']})
+                            if created:
+                                twitch.save()
                             timestamp = parse(stream['created_at'])
                             live = TwitchLive.objects.get_or_create(twitch=twitch, timestamp=timestamp)[0]
                             for notification in twitch.twitchnotification_set.all():
