e6dc70f9e8d74512c077390b76d623ef020d5c1a
Brandon <git@bsquid.io>
2017-12-24 12:53:53 -0800

    More setting changes

--- web/settings.py
+++ web/settings.py
@@ -71,8 +71,7 @@ else:
 
 try:
     import dj_database_url
-    db_from_env = dj_database_url.config()
-    DATABASES['default'].update(db_from_env)
+    DATABASES['default'] = dj_database_url.config(conn_max_age=600)
 except:
     pass
 
