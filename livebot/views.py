0574ec4da9a276833c3f4c942e151cf73bda3c2d
Brandon <git@bsquid.io>
2017-12-27 09:18:05 -0800

    Add current site domain to template variables for reference

--- livebot/views.py
+++ livebot/views.py
@@ -1,4 +1,5 @@
 from django.contrib import messages
+from django.contrib.sites.shortcuts import get_current_site
 from django.shortcuts import render, redirect
 from django.urls import reverse
 from django.utils.decorators import method_decorator
@@ -20,6 +21,10 @@ class BaseLiveBotView(View):
         self.context = {}
         self.context['nbar'] = self.app_label
         super().__init__(*args, **kwargs)
+
+    def dispatch(self, request, *args, **kwargs):
+        self.context['current_site_domain'] = get_current_site(request).domain
+        return super().dispatch(request, *args, **kwargs)
 
     def get(self, request, *args, **kwargs):
         messages.add_message(request, messages.ERROR, 'Page not found!')
